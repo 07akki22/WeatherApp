992e4419e8f1fe8ca6005173941414302d6410b9
07akki22 <157102847+07akki22@users.noreply.github.com>
2024-02-16 13:59:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+async function fetchWeatherData(cityName){
+	let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=244a258b01c648348389a1a5a193eecf`;
+	const response = await fetch(url)
+	const data = await response.json();
+	let city = document.getElementById("city");
+	city.innerHTML = data.name;
+
+
+
+	 let Feel_Like = document.getElementById("Feel_Like");
+	Feel_Like.innerHTML = Math.round((data.main.temp)-273.15)+"°C";
+
+
+
+	let windSpeed = document.getElementById("wind_speed");
+	windSpeed.innerHTML = data.wind.speed + "km/hr";
+	let humidity = document.getElementById("humidity");
+	humidity.innerHTML = data.main.humidity + "g/kg";
+	let temp = document.getElementById("temp");
+	temp.innerHTML = Math.round((data.main.temp)-273.15)+"°C";
+	console.log(data);
+
+}
+
+function fetchCity(){
+	let cityName = document.getElementById("cityname");
+	if(cityName.value===""){
+		alert("Enter a city name");
+
+	}else{
+		fetchWeatherData(cityName.value);
+		cityName.value="";
+
+	}
+}
